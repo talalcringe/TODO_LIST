0d978e4487916d6f488c031146e33aaa8aceb102
talalcringe <talal01471@gmail.com>
2024-09-27 17:49:45 +0500

    feat: add sort and filter

--- client/src/components/Tasks.tsx
+++ client/src/components/Tasks.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useGetTasks } from '../api/taskQueries'; // Import the useGetTasks hook
+import { useGetTasks } from '../api/taskQueries';
 
 import TaskView from './TaskView';
 import CreateTask from './CreateTask';
@@ -8,6 +8,8 @@ import Fab from '@mui/material/Fab';
 import TextField from '@mui/material/TextField';
 import CircularProgress from '@mui/material/CircularProgress';
 
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import ToggleButton from '@mui/material/ToggleButton';
 import Box from '@mui/material/Box';
 import { Typography } from '@mui/material';
 
@@ -16,6 +18,7 @@ type Task = {
   title: string;
   description: string;
   completed: boolean;
+  createdAt: string;
   updatedAt: string;
   duedate?: string;
 };
@@ -27,7 +30,11 @@ function Tasks({
     value: [boolean, string, 'success' | 'error' | 'info' | 'warning']
   ) => void;
 }) {
-  const [filter, setFilter] = useState<string>('');
+  const [search, setSearch] = useState<string>('');
+  const [filter, setFilter] = useState<'all' | 'active' | 'completed'>('all');
+  const [sort, setSort] = useState<'updated' | 'created' | 'duedate'>(
+    'updated'
+  );
   const [creatingTask, setCreatingTask] = useState(false);
 
   const { data: tasks, isLoading, isError } = useGetTasks();
@@ -36,21 +43,53 @@ function Tasks({
     setCreatingTask((prevState) => !prevState);
   }
 
-  function updateFilter(e: React.ChangeEvent<HTMLInputElement>) {
-    setFilter(e.target.value);
+  function updateSearch(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearch(e.target.value);
   }
 
-  const filteredTasks = tasks?.filter((task: Task) =>
-    task.title.toLowerCase().includes(filter.toLowerCase())
-  );
+  function updateFilter(value: 'all' | 'active' | 'completed') {
+    setFilter(value);
+  }
+
+  function updateSort(value: 'updated' | 'created' | 'duedate') {
+    setSort(value);
+  }
+
+  const filteredTasks = tasks?.filter((task: Task) => {
+    if (task.title.toLowerCase().includes(search.toLowerCase())) {
+      if (filter === 'active') {
+        return !task.completed;
+      } else if (filter === 'completed') {
+        return task.completed;
+      }
+      return true;
+    }
+    return false;
+  });
 
   const sortedTasks =
     filteredTasks?.sort((a: Task, b: Task) => {
-      if (a.completed !== b.completed) {
-        return a.completed ? 1 : -1;
+      if (sort === 'updated') {
+        return (
+          new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime()
+        );
+      } else if (sort === 'created') {
+        return (
+          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        );
+      } else if (sort === 'duedate') {
+        if (!a.duedate && !b.duedate) {
+          return (
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+          );
+        } else if (!a.duedate) {
+          return 1;
+        } else if (!b.duedate) {
+          return -1;
+        } else {
+          return new Date(b.duedate).getTime() - new Date(a.duedate).getTime();
+        }
       }
-
-      return new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime();
     }) || [];
 
   if (isError) {
@@ -60,14 +99,91 @@ function Tasks({
 
   return (
     <>
-      <Box mt={'72px'}>
+      <Box
+        mt={'72px'}
+        width={'70%'}
+        display={'flex'}
+        flexDirection={'column'}
+        alignItems={'center'}
+        justifyContent={'space-between'}
+        gap={2}
+        padding={'8px 16px'}
+      >
         <TextField
           size='small'
           label='Search'
-          value={filter}
+          value={search}
           sx={{ bgcolor: 'white' }}
-          onChange={updateFilter}
+          onChange={updateSearch}
         />
+        <Box
+          display={'flex'}
+          gap={1}
+          alignItems={'center'}
+          flexWrap={'wrap'}
+          bgcolor={'#fff'}
+          borderRadius={2}
+          p={1}
+        >
+          <Box display={'flex'} gap={1}>
+            <Typography
+              sx={{ m: 1 }}
+              variant='subtitle1'
+              color={'#007bff'}
+              fontWeight={'bold'}
+              mr={1}
+            >
+              FILTER:
+            </Typography>
+            <ToggleButtonGroup
+              color='primary'
+              value={filter}
+              exclusive
+              onChange={(_e, value) => {
+                updateFilter(value);
+              }}
+            >
+              <ToggleButton value='all' size='small'>
+                All
+              </ToggleButton>
+              <ToggleButton value='active' size='small'>
+                Active
+              </ToggleButton>
+              <ToggleButton value='completed' size='small'>
+                Completed
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </Box>
+          <Box display={'flex'} gap={1}>
+            <Typography
+              sx={{ m: 1 }}
+              variant='subtitle1'
+              color={'#007bff'}
+              fontWeight={'bold'}
+              mr={1}
+            >
+              SORT BY:
+            </Typography>
+            <ToggleButtonGroup
+              color='primary'
+              value={sort}
+              exclusive
+              onChange={(_e, value) => {
+                updateSort(value);
+              }}
+            >
+              <ToggleButton value='updated' size='small'>
+                Updated At
+              </ToggleButton>
+              <ToggleButton value='created' size='small'>
+                Created At
+              </ToggleButton>
+              <ToggleButton value='duedate' size='small'>
+                Due On
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </Box>
+        </Box>
       </Box>
 
       <Box
